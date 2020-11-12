9f4af73eb54e22f179bcf32b09587531057591f5
Railton98 <railtonleal98@gmail.com>
2020-11-12 11:31:39 -0300

    auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'screens/auth_screen.dart';
 import 'screens/chat_screen.dart';
 
 void main() {
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ChatScreen(),
+      // home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
