850ce791d019fb72162821a565e0a5987cbd1a04
Railton98 <railtonleal98@gmail.com>
2020-11-12 12:26:47 -0300

    integrating authentication with firebase - #part 01

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../models/auth_data.dart';
 import '../widgets/auth_form.dart';
@@ -9,14 +11,40 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  void _handleSubmit(AuthData authData) {
-    print(authData.name);
-    print(authData.email);
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final _auth = FirebaseAuth.instance;
+
+  Future<void> _handleSubmit(AuthData authData) async {
+    try {
+      if (authData.isLogin) {
+        await _auth.signInWithEmailAndPassword(
+          email: authData.email.trim(),
+          password: authData.password,
+        );
+      } else {
+        await _auth.createUserWithEmailAndPassword(
+          email: authData.email.trim(),
+          password: authData.password,
+        );
+      }
+    } on PlatformException catch (err) {
+      final msg = err.message ?? 'Ocorreu um erro! Verifique suas Credenciais!';
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text(msg),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+    } catch (err) {
+      print(err);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Theme.of(context).primaryColor,
       body: AuthForm(_handleSubmit),
     );
